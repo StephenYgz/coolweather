333933a75d8e6a914d198c72f0c15e9f73b954b7
ygz <1216203615@qq.com>
2018-02-01 17:04:56 +0800

    bing pic

--- app/src/main/java/com/demo/coolweather/WeatherActivity.java
+++ app/src/main/java/com/demo/coolweather/WeatherActivity.java
@@ -12,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -20,6 +21,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.demo.coolweather.gson.Weather;
 import com.demo.coolweather.util.HttpUtil;
 import com.demo.coolweather.util.Utility;
@@ -32,6 +34,10 @@ import okhttp3.Response;
 
 public class WeatherActivity extends AppCompatActivity {
 
+    /**
+     * 必应美图
+     */
+    private ImageView bingPicImg;
     /**
      * 天气信息最外层布局
      */
@@ -82,8 +88,15 @@ public class WeatherActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= 21) {
+            Window window = getWindow();
+            View decorView = window.getDecorView();
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        }
         setContentView(R.layout.activity_weather);
         // 初始化各控件
+        bingPicImg = ((ImageView) findViewById(R.id.bing_pic_img));
         weatherLayout = (ScrollView) findViewById(R.id.weather_layout);
         titleCity = (TextView) findViewById(R.id.title_city);
         titleUpdateTime = (TextView) findViewById(R.id.title_update_time);
@@ -95,7 +108,9 @@ public class WeatherActivity extends AppCompatActivity {
         comfortText = (TextView) findViewById(R.id.comfort_text);
         carWashText = (TextView) findViewById(R.id.car_wash_text);
         sportText = (TextView) findViewById(R.id.sport_text);
+
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        // 天气信息
         String weatherString = prefs.getString("weather", null);
         if (weatherString != null) {
             // 有缓存时直接解析天气数据
@@ -107,6 +122,16 @@ public class WeatherActivity extends AppCompatActivity {
             mWeatherId = getIntent().getStringExtra("weather_id");
             weatherLayout.setVisibility(View.INVISIBLE);
             requestWeather(mWeatherId);
+        }
+
+        // 必应美图链接
+        String bingPic = prefs.getString("bing_pic", null);
+        if (bingPic != null) {
+            // 有缓存时直接使用Glide加载
+            showBingPic(bingPic);
+        } else {
+            // 无缓存时去服务器获取
+            loadBingPic();
         }
     }
 
@@ -147,6 +172,8 @@ public class WeatherActivity extends AppCompatActivity {
                 });
             }
         });
+
+        loadBingPic();
     }
 
     /**
@@ -187,5 +214,43 @@ public class WeatherActivity extends AppCompatActivity {
 
         // 显示天气信息最外层布局
         weatherLayout.setVisibility(View.VISIBLE);
+    }
+
+    /**
+     * 加载必应美图
+     */
+    private void loadBingPic() {
+        String requestBingPic = "http://guolin.tech/api/bing_pic";
+        HttpUtil.sendOkHttpRequest(requestBingPic, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                final String bingPic = response.body().string();
+                // 必应美图地址缓存到sp中
+                SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(WeatherActivity.this);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString("bing_pic", bingPic);
+                editor.apply();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        showBingPic(bingPic);
+                    }
+                });
+            }
+        });
+    }
+
+    private void showBingPic(String bingPic) {
+        Glide
+                .with(this)
+                .load(bingPic)
+                .crossFade()
+                .diskCacheStrategy(DiskCacheStrategy.SOURCE)
+                .into(bingPicImg);
     }
 }
